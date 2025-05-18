8963f3f8cb2f9ca41cc02ff15dde3d7d020fc539
Wu Xiaotian <yetist@gmail.com>
2025-05-18 21:50:49 +0800

    x

--- .github/workflows/builds.sh
+++ .github/workflows/builds.sh
@@ -54,11 +54,10 @@ if [ -f meson.build ]; then
 	infoend
 
 	infobegin "Dist (meson)"
-	id
-	pwd
-	ls -ltra
-	ls -ltra ..
-	git status
-	#ninja -C _build dist
+	# Git safedirectory stop ninja dist
+	# https://github.com/git/git/commit/8959555cee7ec045958f9b6dd62e541affb7e7d9
+	# https://git-scm.com/docs/git-config/2.35.2#Documentation/git-config.txt-safedirectory
+	git config --global --add safe.directory ${PWD}
+	ninja -C _build dist
 	infoend
 fi
